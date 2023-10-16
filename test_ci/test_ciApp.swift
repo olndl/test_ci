48310e1ac561799c49fee666d8c8a96630df3915
Olga Shiryavceva <olga@newagesports.net>
2023-10-16 16:58:21 +0300

    first commit

---
+++ test_ci/test_ciApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_ciApp.swift
+//  test_ci
+//
+//  Created by Ольга Ширявцева on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct test_ciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
